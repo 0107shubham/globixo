29c59fc2952779f6b6a34544331168c784342d36
shubham tyagi <shubhamtyagi.lncte@gmail.com>
2024-09-13 16:53:40 +0530

    update header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="bg-gray-300 h-[10vh] w-full flex items-center border-b-[5px] border-purple-900">
+    <div className="bg-gray-300 h-[10vh] w-full flex items-center border-b-[2px] border-purple-500">
       <div className="text-2xl font-bold text-purple-600 ml-4 ">Globixo</div>
     </div>
   );
